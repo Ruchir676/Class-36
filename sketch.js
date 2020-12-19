c30f6cbf1b61eb5444309963b5b5622a0842756e
Ruchir676 <72313742+Ruchir676@users.noreply.github.com>
2020-12-19 14:12:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas;
+var gameState= 0;
+var playerCount, database, form, player, game
+
+function setUp() {
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+
+}
